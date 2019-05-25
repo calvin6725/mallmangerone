ee34691c76fcbac784e5427396beac8a914ef8df
calvin6725 <1249563746@qq.com>
2019-05-25 19:21:46 +0800

    显示home组件

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 const MyHttpServer={}
 MyHttpServer.install = function (Vue) {
-    axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+    axios.defaults.baseURL='http://localhost:8888/api/private/v1/';
     // 4. 添加实例方法
     Vue.prototype.$http = axios
   }
