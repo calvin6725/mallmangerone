4da698ba0bb820750fe865630f65b0a7faf5df6b
calvin6725 <1249563746@qq.com>
2019-05-26 02:33:24 +0800

    home退出功能

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,15 +1,142 @@
 <template>
-    <div>
-        home
-    </div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="黑马">
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h3>电商后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+            <a href @click.prevent="handleSignout()" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">
+        <!-- 侧边栏 -->
+        <el-menu
+        :unique-opened="true">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-s-promotion"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>用来列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+              <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+              <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+              <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>选项一</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+    // newVue之前自动触发
+    beforeCreate(){
+        const token=localStorage.getItem('token')
+        if(!token){
+            this.$router.push({name:login})
+        }
+    },
+    methods:{
+        handleSignout(){
+        //1.清除token
+        localStorage.clear()
+        //2.提示
+        this.$message.success("退出成功")
+        //3.来的login组件
+        this.$router.push({name:"login"})
+        }
+    }
+};
 </script>
 
 <style>
-
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+}
+/*  */
+.middle {
+  text-align: center;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
