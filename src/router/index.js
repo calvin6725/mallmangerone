991069fca28a82f635450ac281f164c4bc251be2
calvin6725 <1249563746@qq.com>
2019-05-25 15:57:55 +0800

    新建登录分支登录组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login.vue'
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-
-    }
-  ]
+  routes: [{
+      name:'login',
+      path: '/login',
+      component:Login
+    }]
 })
